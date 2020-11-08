36fb78946415dd0000a42deb9c7b988429dfbf75
todortodorov0 <todor1232001@gmail.com>
2020-11-08 16:08:18 +0000

    Exercise6

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,29 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int a = number;
+        int b = a;
+        int count = 1;
+        assert number > 0 : "Fehler";
+        while ((number / 10) != 0) {
+          count++;
+          number = number / 10;
+        }
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+          array[i] = a % 10;
+          a = a / 10;
+        }
+        int c = 0;
+        for (int j = 0; j < array.length; j++) {
+          c = c * 10;
+          c = c + array[j];
+        }
+        if (c == b) {
+          System.out.println("Palindrome: true");
+        } else {
+          System.out.println("Palindrome: false");
+        }
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,29 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        do {
+          String pass = input.next();
+          int a = 0;
+          boolean valid = false;
+          
+          if (pass.length() < 8) {
+            valid = false;
+            continue;
+          } else {
+            for (int i = 0; i < pass.length(); i++) {
+              if (Character.isDigit(pass.charAt(i))) {
+                a++; 
+              }
+              if (!Character.isLetterOrDigit(pass.charAt(i))) {
+                valid = false;
+                break;
+              }
+            }
+          }
+          if (a >= 2) {
+            break;
+          }
+        } while (true);
+        System.out.println("Password valid!");
     }
 }
